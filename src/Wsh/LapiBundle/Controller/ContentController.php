3bf426ae091249261f3278f84818ec410370a473
Maciej Zubok <greengos@gmail.com>
2013-08-24 22:04:53 +0200

    Added methods for returning FeaturedOffers/HotDeals

--- src/Wsh/LapiBundle/Controller/ContentController.php
+++ src/Wsh/LapiBundle/Controller/ContentController.php
@@ -5,24 +5,33 @@ namespace Wsh\LapiBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\DependencyInjection\Container;
 
 class ContentController extends Controller
 {
-    /**
-     * @Route("/hello/{name}")
-     * @Template()
-     */
-    public function indexAction($name)
+    protected $container;
+
+    public function __construct(Container $container)
     {
-        return array('name' => $name);
+        $this->container = $container;
     }
+
 
     /**
      * Returns array of offerProviderSymbol objects marked as featured
      */
-    public function getFeaturedAction()
+    public function getFeaturedOffers()
     {
+        $em = $this->getDoctrine()->getManager();
+        $offersRepo = $em->getRepository('WshLapiBundle:Offer');
 
+        $featuredOffers = $offersRepo->findBy(array('isFeatured' => 1));
+
+        if(!$featuredOffers){
+            throw new \Exception('There is no offers with "Featured Offers" flag.');
+        }
+
+        return $featuredOffers;
     }
 
     /**
@@ -30,6 +39,15 @@ class ContentController extends Controller
      */
     public function getHotDeals()
     {
+        $em = $this->getDoctrine()->getManager();
+        $offersRepo = $em->getRepository('WshLapiBundle:Offer');
 
+        $hotDealOffers = $offersRepo->findBy(array('isHotDeal' => 1));
+
+        if(!$hotDealOffers){
+            throw new \Exception('There is no offers with "Hot Deal" flag.');
+        }
+
+        return $hotDealOffers;
     }
 }
