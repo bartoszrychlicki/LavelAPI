123d150ee7fc403fac390626e59b390011d7d0d5
Maciej Zubok <greengos@gmail.com>
2013-08-29 17:46:50 +0200

    Added function to handle request from Qtravel SearchApi, moved Transform function.

--- src/Wsh/LapiBundle/OfferProvider/Qtravel/Provider.php
+++ src/Wsh/LapiBundle/OfferProvider/Qtravel/Provider.php
@@ -9,11 +9,12 @@
 
 namespace Wsh\LapiBundle\OfferProvider\Qtravel;
 
-
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\DependencyInjection\Container;
 use Wsh\LapiBundle\Entity\Offer;
+use Wsh\LapiBundle\Entity\OfferReadStatus;
 use Wsh\LapiBundle\OfferProvider\OfferProviderInterface;
+use Symfony\Component\Stopwatch\Stopwatch;
 
 class Provider implements OfferProviderInterface
 {
@@ -22,10 +23,14 @@ class Provider implements OfferProviderInterface
     protected $apiGetRequestUrl;
     protected $lastSentRequestUrl;
     protected $apiOfferRequestUrl;
+    protected $buzz;
+    protected $doctrine;
 
     function __construct(Container $container)
     {
         $this->container = $container;
+        $this->buzz = $this->container->get('buzz.browser');
+        $this->doctrine = $container->get("doctrine")->getManager();
         $apiKey = $this->container->getParameter('qtravelApiKey');
         $apiRequestUrl = 'http://api.qtravel.pl/json/apis?qapikey='.$apiKey;
 
@@ -68,8 +73,7 @@ class Provider implements OfferProviderInterface
 
     public function sendRequest($url)
     {
-        $buzz = $this->container->get('buzz.browser');
-        $response = $buzz->get($url);
+        $response = $this->buzz->get($url);
         $this->setLastSentRequestUrl($url);
         return $response->getContent();
     }
@@ -90,39 +94,87 @@ class Provider implements OfferProviderInterface
         return $this->lastSentRequestUrl;
     }
 
-    public function transformToEntity($response) {
-        // decode JSON
+    public function handleOfferResponse($response) {
+        $offerRepo = $this->doctrine->getRepository('WshLapiBundle:Offer');
+
         $json = json_decode($response);
-        if(!($json->offers->o)) {
-            throw new \Exception('Response does not have any offers added to JSON object in '.__FUNCTION__);
-        }
-        if(count($json->offers->o) <= 0) {
-            throw new \Exception('Array with offers on JSON response object is empty in '.__FUNCTION__);
-        }
-        // now iterate over each
-        $collection = new ArrayCollection();
-        foreach($json->offers->o as $offer) {
-            $offerEnt = new Offer();
-            $offerEnt->setName(strip_tags($offer->o_details->o_name));
-            if(!empty($offer->o_details->o_city)) {
-                $offerEnt->setCity(strip_tags($offer->o_details->o_city));
+        if(!empty($json)) {
+            if(!($json->offers->o)) {
+                throw new \Exception('Response does not have any offers added to JSON object in '.__FUNCTION__);
             }
+            if(count($json->offers->o) <= 0) {
+                throw new \Exception('Array with offers on JSON response object is empty in '.__FUNCTION__);
+            }
+            // now iterate over each
+            $collection = new ArrayCollection();
+            foreach($json->offers->o as $offer) {
+                $checkSum = md5(serialize($offer));
+                $offerDB = $offerRepo->findOneByQTravelOfferId($offer->o_details->o_code);
+
+                if($offerDB){
+                    if($offerDB->getCheckSum() != $checkSum){
+                        $this->doctrine->remove($offerDB);
+                        $this->doctrine->flush();
+                        $collection->add($this->transformToEntity($offer));
+                    } else {
+                        $collection->add($offerDB);
+                    }
+                } else {
+                    $collection->add($this->transformToEntity($offer));
+                }
+            }
+            return $collection;
+        } else {
+            return null;
+        }
+    }
+
+    public function transformToEntity($offer)
+    {
+        $offerEnt = new Offer();
+        $offerEnt->setName(strip_tags($offer->o_details->o_name));
+        if(!empty($offer->o_details->o_city)) {
+            $offerEnt->setCity(strip_tags($offer->o_details->o_city));
+        }
+
+        if(!empty($offer->o_details->o_country)) {
             $offerEnt->setCountry(strip_tags($offer->o_details->o_country));
-            if(!empty($offer->o_details->o_departs)) {
+        }
+
+        if(!empty($offer->o_details->o_desc)) {
+            $offerEnt->setDescription(strip_tags($offer->o_details->o_desc));
+        }
+
+        $offerEnt->setIsHotDeal(false);
+        $offerEnt->setIsFeatured(false);
+
+        if(!empty($offer->o_photos)) {
+            $offerEnt->setLeadPhoto($offer->o_photos->o_photo_link[0]);
+        }
+
+        $offerEnt->setPrice($offer->o_details->o_bprice);
+        $offerEnt->setQTravelOfferId($offer->o_details->o_code);
+
+        if(count($offer->o_best->o_b_period) == 1) {
+            $offerEnt->setDuration(array($offer->o_best->o_b_period));
+        } else {
+            $offerEnt->setDuration($offer->o_best->o_b_period);
+        }
+
+        if(!empty($offer->o_details->o_departs)) {
+            if(count($offer->o_details->o_departs->o_depart) == 1) {
+                $offerEnt->setDeparts(array($offer->o_details->o_departs->o_depart));
+            } else {
                 $offerEnt->setDeparts($offer->o_details->o_departs->o_depart);
             }
-            $offerEnt->setDescription(strip_tags($offer->o_details->o_desc));
-            $offerEnt->setIsHotDeal(false);
-            $offerEnt->setIsFeatured(false);
-            $offerEnt->setLeadPhoto($offer->o_photos->o_photo_link[0]);
-            $offerEnt->setPrice($offer->o_details->o_bprice);
-            $offerEnt->setQTravelOfferId($offer->o_details->o_code);
-            $offerEnt->setDuration($offer->o_best->o_b_period);
-
-            $collection->add($offerEnt);
         }
 
-        return $collection;
+        /**
+         * checkSum will be used to check changes in offer
+         */
+        $offerEnt->setCheckSum(md5(serialize($offer)));
+
+        return $offerEnt;
     }
 
     public function transformSingleOfferToEntity($response, $isFeatured = false, $isHotDeal = false) {
