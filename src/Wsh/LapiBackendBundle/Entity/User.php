3cdc06615eaeec3f6af5d8779de8272ce584dfe3
Maciej Zubok <greengos@gmail.com>
2013-08-31 16:07:46 +0200

    Added Entity for user/group to backend.

---
+++ src/Wsh/LapiBackendBundle/Entity/User.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * This file is part of the <name> project.
+ *
+ * (c) <yourname> <youremail>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Wsh\LapiBackendBundle\Entity;
+
+use Sonata\UserBundle\Entity\BaseUser as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="admin_user")
+ * @ORM\Entity()
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * Get id
+     *
+     * @return integer $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
